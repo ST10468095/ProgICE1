cf05a70edc119396d208081af75dca5246afa9e9
lab_services_student <lab_services_student@LabVM2047235>
2026-02-20 20:27:25 +0200

    Add project files.

---
+++ ProgICE1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//student name
+Console.Write("Enter Student Name: ");
+var studentName = Console.ReadLine();
+
+//preferred programming language
+Console.Write("Enter Preferred Programming Language: ");
+string? languageInput = Console.ReadLine();
+
+//name= caps
+var formattedName = studentName?.ToUpper();
+
+// C# if nothung is typed
+var formattedLanguage = string.IsNullOrWhiteSpace(languageInput)
+    ? "C#"
+    : languageInput.Trim();
+
+//store
+var message = $"Student {formattedName} has registered and prefers {formattedLanguage}.";
+
+
+Console.WriteLine(message);
